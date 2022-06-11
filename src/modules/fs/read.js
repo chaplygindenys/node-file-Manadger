a454936de61d75bb49de304a849715a897733f93
chaplygindenys <chaplygindenys@gmail.com>
2022-06-11 17:25:16 +0300

    cat done

--- src/modules/fs/read.js
+++ src/modules/fs/read.js
@@ -1,12 +1,18 @@
-import { lstatSync, readFileSync } from "fs";
-import { cl } from "../../app.mjs";
+import { lstatSync } from "fs";
 import { errMes } from "../../config.js";
+import { MyReadSt } from "./MyReadSt.js";
+import { pipeline } from "stream";
+import { stdout } from "process";
 
 export const read = (path) => {
   try {
     if (lstatSync(path).isFile()) {
-      const buffer = readFileSync(path, "utf8");
-      process.stdout.write(buffer);
+      const readFromFilePath = new MyReadSt(path);
+      pipeline(readFromFilePath, stdout, (err) => {
+        if (err) {
+          throw new Error(err);
+        }
+      });
       return "ok";
     } else {
       return errMes();
